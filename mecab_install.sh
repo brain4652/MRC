256d270afd87ee2e61f76eec2cd512e7bb2e32a5
LEE KWANGHO <85268213+brain4652@users.noreply.github.com>
2022-06-03 17:54:49 +0900

    Add files via upload

---
+++ mecab_install.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+sudo apt-get install g++ openjdk-8-jdk python3-dev python3-pip curl
+pip install konlpy
+pip install --upgrade pip
+pip3 install JPype1-py3
+mkdir mecab
+cd mecab
+wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
+tar zxfv mecab-0.996-ko-0.9.2.tar.gz
+cd mecab-0.996-ko-0.9.2
+./configure
+make
+make check
+sudo make install
+cd ..
+wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
+tar zxfv mecab-ko-dic-2.1.1-20180720.tar.gz
+cd mecab-ko-dic-2.1.1-20180720
+./autogen.sh
+./configure
+make
+sudo make install
